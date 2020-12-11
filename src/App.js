86fd1bbd1d8808158b3a8e9a579f7418fe884144
Illia <illia2143@gmail.com>
2020-12-10 22:11:18 -0500

    user input added + btn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [playerName, setPlayerName] = useState('');
+  const elements = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+  const [sortedElements, changeOrder] = useState(elements);
+
+  const randomizeArr = (array) => {
+    return array.sort( () => Math.random()-.5).concat([0]);
+  };
+
+  useEffect(() => {
+    randomizeArr(elements);
+  }, []);
+
+
+  const checkUserName = function(name) {    
+    if (!name) {
+      alert('Invalid name')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="">
+        <input type="text" onChange={ e => setPlayerName(e.target.value) } value={playerName} />
+        <button onClick={() => checkUserName(playerName)}>Start</button>
+      </form>
     </div>
   );
 }
