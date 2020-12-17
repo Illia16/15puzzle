cfedd0068ba4c15d891f4e8703a14d2e30d17eb2
Illia <illia2143@gmail.com>
2020-12-16 20:49:57 -0500

    imported separate function for checking name added

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,9 @@ import playersStats from './playersStats.js';
 // Timer functions
 import { startTimer, stopTimer, timerInit, activeTimeSec } from './helpers/timer.js';
 
+// Check name
+import { checkName } from './helpers/checkName.js';
+
 // Game logic
 import { movementsMade, makeMove, updateArray} from './logic/makeMove.js';
 import { placeCells } from './logic/gameBoard.js';
@@ -11,13 +14,13 @@ import { placeCells } from './logic/gameBoard.js';
 // DOM elements
 const startGameDOM = document.getElementById('startGame');
 const gameOverDOM = document.getElementById('done');
-const playerName = document.getElementById('name').value;
+const playerName = document.getElementById('name');
 document.querySelector(".gameBoard").addEventListener('click', () => updateArray(cells));
 
 // Game info
 // NEED TO KEEP EVERYTHING HERE: currentPlayerName, activeTimeSec, movementsMade to later push into Firebase once game is over.
 const stats = playersStats;
-const currentPlayerName = (name) => name;
+let currentPlayerName;
 const cells = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15].sort( () => Math.random()-0.5).concat(0);
 let finalTime = (timeFinal) => timeFinal;
 let finalMovementsCount = (movementsCount) => movementsCount;
@@ -25,10 +28,10 @@ let finalMovementsCount = (movementsCount) => movementsCount;
 startGameDOM.onclick = function(e) {
     e.preventDefault();
     
-    if (!playerName || playerName.length > 15) {
+    if (!checkName(playerName.value) || !isNaN(checkName(playerName.value)) ) {
         alert('Invalid name');
     } else {
-        currentPlayerName(playerName);
+        currentPlayerName = checkName(playerName.value);
         placeCells(cells);
         startTimer();
     };
