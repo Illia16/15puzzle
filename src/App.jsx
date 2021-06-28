4bec2317eb0a07c9695c90ef83abc9deb021c35e
Illia16 <illia2143@gmail.com>
2021-06-27 21:59:08 -0400

    d

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import './styles/index.scss';
 
 function App() {
   const [playersData, setPlayersData] = useState(null);
-  
-  useEffect(()=> {
-      try {
-        fetch("API URL HERE")
-        .then(res => res.json())
-        .then(data => {
-          setPlayersData(data)
-        })
-      } catch (er) {
-        console.error(er);
-      }
-  },[])
+
+    useEffect(()=> {
+        try {
+          fetch("***********",
+            {
+              headers: {
+                "Content-Type": "application/json",
+                "x-api-key": import.meta.env.VITE_API_KEY,
+              }
+            }
+          )
+          .then(res => res.json())
+          .then(data => {
+            setPlayersData(data)
+            console.log(data);
+          })
+        } catch (er) {
+          console.error(er);
+        }
+    },[])
 
 
   return (
