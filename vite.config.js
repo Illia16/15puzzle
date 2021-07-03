5c42f3a3416bc4153c8aa153983cf8be84cd5031
Illia16 <illia2143@gmail.com>
2021-07-02 22:37:34 -0400

    Vite removed, init logic

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import vite from "vite";
-
-const config = vite.defineConfig({
-  base: '/15puzzle/',
-  plugins: [reactRefresh()],
-});
-
-export default config;
