5f945f9e4229fb788c30912e94de3122cfdf7356
Illia16 <illia2143@gmail.com>
2021-07-02 22:37:34 -0400

    Vite removed, init logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,63 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './styles/index.scss';
+import React from "react";
+import { useEffect, useState } from "react";
+import "./styles/index.scss";
 
 function App() {
   const [playersData, setPlayersData] = useState(null);
+  const [playerName, setPlayerName] = useState("");
+  const [gameStarted, setGameStarted] = useState(false);
 
-
-    useEffect(()=> {
-        try {
-          fetch("https://xo3o941k2f.execute-api.us-east-2.amazonaws.com/production/game15-api",
-            {
-              headers: {
-                "x-api-key": import.meta.env.VITE_API_KEY || secret.VITE_API_KEY,
-              }
-            }
-          )
-          .then(res => res.json())
-          .then(data => {
-            setPlayersData(data)
-            console.log(data);
-          })
-        } catch (er) {
-          console.error(er);
-        }
-
-        // try {
-        //   fetch("https://xo3o941k2f.execute-api.us-east-2.amazonaws.com/production/game15-api", {
-        //     method: 'POST',
-        //     headers: {
-        //         "x-api-key": import.meta.env.VITE_API_KEY,
-        //     },
-        //     body: JSON.stringify({moves: '1', time: '1', id: '1', name: 'Illia'}),
-        //   })
-        //   .then(res => res.json())
-        //   .then(data => {
-        //     setPlayersData(data)
-        //     console.log(data);
-        //   })
-        // } catch (er) {
-        //   console.error(er);
-        // }
-    },[])
-
+  const startGame = () => {
+    if (playerName) {
+      setGameStarted(true);
+    }
+  };
+  
+  useEffect(() => {
+    // try {
+    //   fetch("https://xo3o941k2f.execute-api.us-east-2.amazonaws.com/production/game15-api",
+    //     {
+    //       headers: {
+    //         "x-api-key": REACT_APP_API_KEY || secret.VITE_API_KEY,
+    //       }
+    //     }
+    //   )
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     setPlayersData(data)
+    //     console.log(data);
+    //   })
+    // } catch (er) {
+    //   console.error(er);
+    // }
+    // try {
+    //   fetch("https://xo3o941k2f.execute-api.us-east-2.amazonaws.com/production/game15-api", {
+    //     method: 'POST',
+    //     headers: {
+    //         "x-api-key": REACT_APP_API_KEY,
+    //     },
+    //     body: JSON.stringify({moves: '1', time: '1', id: '1', name: 'Illia'}),
+    //   })
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     setPlayersData(data)
+    //     console.log(data);
+    //   })
+    // } catch (er) {
+    //   console.error(er);
+    // }
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-7xl">App</h1>
+        <h1 className="text-7xl">15 Game</h1>
+        <div className="userName">
+          <input type="text" onChange={(e) => setPlayerName(e.target.value)} />
+          <button onClick={startGame}>Ok</button>
+        </div>
+
+        <div>{playerName}</div>
       </header>
     </div>
   );
