eb8071bb5cfdc63dd7b67747fd765e38449c554b
Illia16 <illia2143@gmail.com>
2021-07-04 22:11:32 -0400

    game is complete

---
+++ src/components/checkIfCellMovable.js
@@ -0,0 +1,5 @@
+const checkIfCellMovable = (clickedCellPositionData, holePositionData) => {
+  return (clickedCellPositionData.x-holePositionData.x<=1 && clickedCellPositionData.y-holePositionData.y===0) || (clickedCellPositionData.x-holePositionData.x===0 && clickedCellPositionData.y-holePositionData.y<=1)
+};
+
+export default checkIfCellMovable;
