003fb4714e2431ee912c2abed249e9ecd283a5ae
Illia16 <illia2143@gmail.com>
2021-07-07 18:31:03 -0400

    fix for cells that were movable if there are before a hole in the same row/column

--- src/components/checkIfCellMovable.js
+++ src/components/checkIfCellMovable.js
@@ -1,5 +1,10 @@
 const checkIfCellMovable = (clickedCellPositionData, holePositionData) => {
-  return (clickedCellPositionData.x-holePositionData.x<=1 && clickedCellPositionData.y-holePositionData.y===0) || (clickedCellPositionData.x-holePositionData.x===0 && clickedCellPositionData.y-holePositionData.y<=1)
+  return ( 
+            (clickedCellPositionData.x-holePositionData.x===1 || clickedCellPositionData.x-holePositionData.x===-1) 
+            && clickedCellPositionData.y-holePositionData.y===0) 
+            || (clickedCellPositionData.x-holePositionData.x===0 
+            && 
+            (clickedCellPositionData.y-holePositionData.y===1 || clickedCellPositionData.y-holePositionData.y===-1))
 };
 
 export default checkIfCellMovable;
