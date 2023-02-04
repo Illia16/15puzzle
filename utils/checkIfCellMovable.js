9d46be2333a457214808620f28731eb56133a280
Illia16 <illia2143@gmail.com>
2023-02-03 22:20:09 -0500

    java 15 game init

--- utils/checkIfCellMovable.js
+++
@@ -1,5 +0,0 @@
-const checkIfCellMovable = (cell, hole) => {
-  return ( (Math.abs(cell.x-hole.x) ===1) && cell.y-hole.y===0) || (cell.x-hole.x===0 && (Math.abs(cell.y-hole.y) ===1) );
-};
-
-export default checkIfCellMovable;
