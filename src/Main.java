2bf5491a1dad9684c576157786f29048a04f77d7
Illia16 <illia2143@gmail.com>
2023-03-15 22:17:28 -0400

    more logic

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,55 @@
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Scanner;
+
 public class Main {
+    static int[] arr = generateArray();
     public static void main(String[] args) {
-        System.out.println(generateArray());
+        getInput();
     }
 
     public static int[] generateArray() {
-        int[] arr = {1,2,3};
+        int[] arr = new int[16];
+        for (int i = arr.length-1; i > 0; i--) {
+            arr[i] = i;
+        }
+
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = arr.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            int a = arr[index];
+            arr[index] = arr[i];
+            arr[i] = a;
+        }
+
         return arr;
+    }
+
+    public static void getInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        if (scanner.hasNextInt()) {
+            System.out.println("Number");
+            String input = scanner.nextLine();
+
+            if (checkForNumber(input)) {
+                System.out.println("Number is present!");
+//                TO DO:
+//                1) Check if movable
+//                2) Move if movable, else return
+//                3) Check if game is over
+                getInput();
+            } else {
+                System.out.println("Number is not present!");
+                getInput();
+            }
+        } else {
+            System.out.println("NaN");
+            getInput();
+        }
+    }
+
+    public static boolean checkForNumber(String v) {
+        return Arrays.stream(arr).anyMatch(i -> i == Integer.parseInt(v));
     }
 }
